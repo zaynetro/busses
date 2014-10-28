1a83071e3c6fe8aedca4769ba0fc8a0b89352794
Roman Zaynetdinov <romanznet@gmail.com>
2014-10-28 15:24:53 +0200

    Make basic react app

--- Gemfile
+++ Gemfile
@@ -37,3 +37,6 @@ gem 'spring',        group: :development
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+
+# Use ES6 modules
+# gem 'es6_module_transpiler-rails'
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -23,21 +23,90 @@ var xhr = function (method, url, cb) {
 
 };
 
+var sampleStops = [
+  {
+    created_at: "2014-10-28T08:17:26.855Z",
+    id: 1,
+    name: "Ritzinkuja",
+    number: 1616,
+    updated_at: "2014-10-28T08:17:26.855Z"
+  },
+  {
+    created_at: "2014-10-28T08:30:03.514Z",
+    id: 3,
+    name: "Kesakatu",
+    number: 42,
+    updated_at: "2014-10-28T08:30:03.514Z"
+  }
+];
+
 window.onload = function () {
 
-  var searchEl = document.querySelector('#search');
-  var stopsEl = document.querySelector('#stops');
+  /*var Timetable = React.createClass({ displayName : 'Timetable',
 
-  searchEl.addEventListener('keyup', function () {
+  });*/
 
-    var num = searchEl.value;
+  var Stop = React.createClass({ displayName : 'Stop',
 
-    xhr('GET', '/stops.json?number=' + num, function (err, data) {
-      if(err) return console.log(err);
+    propTypes : {
+      number : React.PropTypes.number.isRequired,
+      name : React.PropTypes.string
+    },
 
-      data = JSON.parse(data);
-      stopsEl.innerText = JSON.stringify(data, null, '\t');
-    });
-  }, false);
+    onClick : function (e) {
+      e.preventDefault();
+      console.log(this.props.id);
+    },
+
+    render : function () {
+      return (
+        React.DOM.li(null,
+          React.DOM.a({
+              href : '#' + this.props.number,
+              onClick : this.onClick,
+              key : this.props.id
+            }, this.props.number + ' - ' + this.props.name)
+        )
+      );
+    }
+  });
+
+
+  var Stops = React.createClass({ displayName : 'Stops',
+    render : function () {
+      return React.DOM.ul({ className : 'bus-stops' }, this.props.items.map(Stop));
+    }
+  });
+
+  var App = React.createClass({ displayName : 'App',
+    getInitialState : function () {
+      return { items : sampleStops };
+    },
+
+    onKeyUp : function (e) {
+      var self = this;
+      xhr('GET', '/stops.json?number=' + e.target.value, function (err, data) {
+        if(err) return console.log(err);
+        self.setState({ items : JSON.parse(data) });
+      });
+    },
+
+    render : function () {
+      return (
+        React.DOM.div(null,
+          React.DOM.h3(null, 'Search'),
+          React.DOM.input({
+            onKeyUp : this.onKeyUp,
+            placeholder : 'Start typing bus stop'
+          }),
+          Stops({ items : this.state.items })
+        )
+      );
+    }
+  });
+
+  React.renderComponent(App(null), document.querySelector('#app'));
 
 };
+
+
