0f0596d96f6cf45033ad3bdcb8485adde12b6584
Roman Zaynetdinov <romanznet@gmail.com>
2014-10-28 13:11:54 +0200

    Add bower

--- Gemfile
+++ Gemfile
@@ -37,5 +37,3 @@ gem 'spring',        group: :development
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
-
-gem "bower-rails", "~> 0.9.1"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # include Bower components in compiled assets
+  config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+
 end
